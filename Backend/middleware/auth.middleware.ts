48d3559e9fe31053bdeffbef3db43ca786235e9e
habtesh-beki <habteshbaki2014@gmail.com>
2025-06-21 18:08:44 -0400

    add middleware for authentication

---
+++ Backend/middleware/auth.middleware.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Response, Request } from "express";
+import { findOne } from "../DB/repositories/user.repositories";
+import { verifyToken } from "../utils/jwt";
+
+export const authenticateJWT = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+  if (!token) {
+    res.status(403).json({ message: "Access denied. No token provided." });
+    return;
+  }
+  const decoded = verifyToken(token);
+  if (!decoded) {
+    res.status(401).json({ message: "Invalid token" });
+    return;
+  }
+
+  try {
+    const email = decoded;
+    const user = await findOne(email);
+    console.log(user);
+    if (!user) {
+      res.status(404).json({
+        message: "user not found",
+      });
+      return;
+    }
+
+    // req.user = user;
+    next();
+  } catch (err) {
+    res.status(500).json({
+      message: "Error fetching student data",
+    });
+  }
+};
