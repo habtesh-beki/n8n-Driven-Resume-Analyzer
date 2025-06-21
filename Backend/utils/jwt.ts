92d88fa6bf649f85a4777bd47b6e334e157cd40e
habtesh-beki <habteshbaki2014@gmail.com>
2025-06-21 18:11:31 -0400

    add jwt functionality

---
+++ Backend/utils/jwt.ts
@@ -0,0 +1,20 @@
+import { configDotenv } from "dotenv";
+import jwt from "jsonwebtoken";
+configDotenv();
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+console.log(JWT_SECRET);
+export const signToken = async (id: string) => {
+  const token = jwt.sign({ id }, JSON.stringify(JWT_SECRET), {
+    expiresIn: 60 * 60 * 60,
+  });
+  return token;
+};
+
+export const verifyToken = (token: string): any => {
+  try {
+    return jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return null;
+  }
+};
