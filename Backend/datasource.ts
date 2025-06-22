afc6b92f040d62791fd539057bf94005b99b10bd
habtesh-beki <habteshbaki2014@gmail.com>
2025-06-22 11:29:27 -0400

    change the imported file

--- Backend/datasource.ts
+++ Backend/datasource.ts
@@ -1,14 +1,17 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./DB/schema/user.schema";
+import { configDotenv } from "dotenv";
+import { User } from "./DB/schema/user.schema.js";
 
+configDotenv();
+console.log(process.env.POSTGRES_HOST);
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "habtesh01",
-  database: "n8n-resume-analyzer",
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || "5432"),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: [User],
   synchronize: true,
 });
