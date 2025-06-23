f9339970c7b17ba451a911f28398abe18dcad803
habtesh-beki <habteshbaki2014@gmail.com>
2025-06-23 03:14:33 -0400

    fix the error

--- Backend/utils/jwt.ts
+++ Backend/utils/jwt.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.POSTGRES_DB as string;
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export const signToken = async (id: string) => {
   console.log(JWT_SECRET);
