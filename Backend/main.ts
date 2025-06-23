aa12ed0a0c9bae38ac69887669b474b136b4f7bb
habtesh-beki <habteshbaki2014@gmail.com>
2025-06-23 03:12:53 -0400

    change the dot env config

--- Backend/main.ts
+++ Backend/main.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from "express";
 import router from "./routes/routes.js";
-import { configDotenv } from "dotenv";
+// import { config as configDotenv } from "dotenv";
+import dotenv from "dotenv";
 import { initialize } from "./Intialize.js";
+
+dotenv.config();
+
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
-configDotenv();
+console.log("postgres_user", process.env.POSTGRES_USER);
 app.use(express.json());
 initialize();
 app.use("/api", router);
