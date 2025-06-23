60af2cf99e38863158e03b70fb6d3aacc65fed70
habtesh-beki <habteshbaki2014@gmail.com>
2025-06-23 03:12:25 -0400

    change the value in to .env value

--- Backend/datasource.ts
+++ Backend/datasource.ts
@@ -1,17 +1,14 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { configDotenv } from "dotenv";
 import { UserTable } from "./DB/schema/user.schema.js";
 
-configDotenv();
-console.log(process.env.POSTGRES_HOST);
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "postgres",
-  port: 5432,
-  username: "postgres",
-  password: "habtemariam",
-  database: "resume_analysis",
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: [UserTable],
   synchronize: true,
 });
